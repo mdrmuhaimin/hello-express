7ae9c8cc765d89dfd4407342a090b66656a7ca84
Muhammad Muhaimin <muhammad.r.muhaimin@gmail.com>
2016-01-03 16:34:45 -0500

    Completion of register and get status

--- app.js
+++ app.js
@@ -6,6 +6,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var https = require("https");
+var mongoose   = require('mongoose');
+var autoIncrement = require('mongoose-auto-increment');
+
 
 // Configuration
 var app = express();
@@ -13,28 +16,128 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 var port = process.env.PORT || 8080;        // set our port
+
+var mongoose   = require('mongoose');
+var connection = mongoose.connect('mongodb://nodeapp:password@ds037195.mongolab.com:37195/heroku_ll6jsgr2'); // connect to our database
+autoIncrement.initialize(connection);
+
+var Customer = require('./app/models/customer')(autoIncrement);
+
+
+//==== Router ====//
 var router = express.Router();
 
 router.use(function(req, res, next) {
 
   // log each request to the console
-  console.log(req.method, req.url);
+  //console.log(req);
 
   //Ideally this API should be a secure one and authentication could be done here
-
+  //todo: Authenticate the user for using API
   next();
 });
+
 
 router.get('/', function(req, res) {
   res.json({ message: 'hooray! welcome to our api!' });
 });
 
-app.use('/api', router);
+router.route('/customers')
 
+  // create a customer
+  .post(function(req, res) {
+    var customer = new Customer();
+    customer.email = req.body.email;
+    console.log(customer);
+    customer.save(function(err, customer) {
+      if (err){
+        console.log(err);
+        res.status(400);
+        if(err.code === 11000){
+          return res.send({ message: 'Email already exist in database.'});
+        }
+        if(err.name === "ValidationError"){
+          return res.send({ message: 'Invalid email address'});
+        }
+        return res.send({ message: err });
+      }
+      res.status(201);
+      return res.send({ ticket: customer.ticketNum });
+    });
+  });
 
-//app.get('/', function (req, res) {
-//  res.send('Hello World!');
-//});
+router.route('/customers/:customer_ticket')
+
+  .get(function(req, res) {
+    req.params.customer_ticket = parseInt(req.params.customer_ticket, 10);
+    if(!Number.isInteger(req.params.customer_ticket)){
+      res.status(400);
+      return res.send({ error: "Ticket should be an integer." });
+    }
+    Customer.find({ticketNum: req.params.customer_ticket}, function(err, customer) {
+      if (err)
+        return res.send(err);
+      if( customer.length === 0 ){
+        res.status(404);
+        return res.send({ error: "Ticket not found" });
+      }
+      customer = customer[0];
+      if(customer.isReady){
+        return res.json({ status: "READY" });
+      }
+      return res.json({ status: "NOT READY" });
+    });
+  })
+
+  .put(function(req, res) {
+    req.params.customer_ticket = parseInt(req.params.customer_ticket, 10);
+    if(!Number.isInteger(req.params.customer_ticket)){
+      res.status(400);
+      return res.send({ error: "Ticket should be an integer." });
+    }
+    Customer.find({ticketNum: req.params.customer_ticket}, function(err, customer) {
+      if (err)
+        return res.send(err);
+      if( customer.length === 0 ){
+        res.status(404);
+        return res.send({ error: "Ticket not found" });
+      }
+      customer = customer[0];
+      if( (req.body.status).toUpperCase() === "READY"){
+        customer.isReady = true;
+      }
+      customer.save(function(err) {
+        if (err) {
+          return res.send(err);
+        }
+        res.json({ message: 'Customer updated!' });
+      });
+      console.log(customer);
+      //if(customer.isReady){
+      //  return res.json({ status: "READY" });
+      //}
+      //return res.json({ status: "NOT READY" });
+    });
+    //Customer.find({ticketNum: req.params.customer_ticket}, function(err, customer) {
+    //  if (err)
+    //    return res.send(err);
+    //  if( customer.length === 0 ){
+    //    res.status(404);
+    //    return res.send({ error: "Ticket not found" });
+    //  }
+    //  console.log(customer.isReady);
+    //  if( (req.body.status).toUpperCase() === "READY"){
+    //    customer.isReady = true;
+    //  }
+    //  customer.save(function(err) {
+    //    if (err)
+    //      res.send(err);
+    //    res.json({ message: 'Bear updated!' });
+    //  });
+  });
+
+//Prefix our routes with api and then version
+app.use('/api/v1', router);
 
 var server = app.listen(port, function () {
   var host = server.address().address;
---
+++ app/services/email.js
@@ -0,0 +1,3 @@
+/**
+ * Created by muhammadmuhaimin on 2016-01-03.
+ */
