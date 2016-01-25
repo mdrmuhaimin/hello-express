a5f016fe643ec8f83860a71a332fec42f3c652cc
Muhammad Raihan Muhaimin <RummanCanon@Muhammads-MacBook-Pro.local>
2016-01-25 18:15:05 -0500

    Added grunt based test

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+  // Do grunt-related things in here
+ grunt.initConfig({
+   pkg: grunt.file.readJSON('package.json'),
+   express: {
+    test: {
+      options: {
+        port: 8080,
+        script: './app.js'
+      }
+    }
+  },
+  mochaTest: {
+      test: {
+        options: {
+        },
+        src: ['test/**/*.js']
+      }
+    }
+ });
+ grunt.loadNpmTasks('grunt-express-server');
+ grunt.loadNpmTasks('grunt-mocha-test');
+ grunt.registerTask('test', ['express:test', 'mochaTest']);
+};
