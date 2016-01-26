9bca511682777615bad3aaecd7cc8eb601787424
Muhammad Raihan Muhaimin <RummanCanon@Muhammads-MacBook-Pro.local>
2016-01-25 19:13:29 -0500

    Sample commit

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ router.use(function(req, res, next) {
 router.route('/tests')
   .get(function(req, res){
     res.status(200);
-    return res.send({ status: "Actice" });
+    return res.send({ status: "Activating" });
   });
 
   router.route('/hello')
